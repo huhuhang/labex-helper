d4f3453edceac4a219fd9d4d038205d38039bee2
huhuhang <no-reply@huhuhang.com>
2025-03-07 09:03:20 +0800

    Bump version to 1.6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LabEx Helper
 // @namespace    http://tampermonkey.net/
-// @version      1.5
+// @version      1.6
 // @description  Helper script for labex.io website
 // @author       huhuhang
 // @match        https://labex.io/*
